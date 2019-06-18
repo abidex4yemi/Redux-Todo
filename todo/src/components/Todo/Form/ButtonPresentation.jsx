56170180416a2c0cec4b4cc1d444220d99ade5b7
abidex4yemi <abidex4yemi@gmail.com>
2019-06-18 15:37:51 +0100

    Create button component

---
+++ todo/src/components/Todo/Form/ButtonPresentation.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const ButtonPresentation = ({ labelText, type, onClick }) => {
+	return (
+		<button type={type} onClick={onClick}>
+			{labelText}
+		</button>
+	);
+};
+
+ButtonPresentation.propTypes = {
+	labelText: PropTypes.string.isRequired,
+	onClick: PropTypes.func.isRequired
+};
+
+ButtonPresentation.defaultProps = {
+	type: 'text'
+};
