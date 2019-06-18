a0d255e39d9e7afde85c8832cb2f1c0cef7c8219
abidex4yemi <abidex4yemi@gmail.com>
2019-06-18 20:36:19 +0100

    add utility function (loadStateFromLocalStorage)

---
+++ todo/src/util/localstorage.js
@@ -0,0 +1,12 @@
+export const loadStateFromLocalStorage = state => {
+	try {
+		const serializedState = localStorage.getItem('state');
+		if (serializedState === null) {
+			return undefined;
+		}
+
+		return JSON.parse(serializedState);
+	} catch (err) {
+		return undefined;
+	}
+};
